3953a08cb976ce93796b0d9317af51c1ab2d1e56
leompe8907 <veyqpasa1312@gmail.com>
2025-11-05 11:58:55 -0300

    migracion de proyecto a uno base

--- udid/apps.py
+++ udid/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class UdidConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'udid'
-
-def ready(self):
-    import udid.signals 
--- udid/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from .consumers import AuthWaitWS
-
-# Rutas de WebSocket para Channels
-websocket_urlpatterns = [
-    # ws://<host>/ws/auth/
-    re_path(r"^ws/auth/$", AuthWaitWS.as_asgi()),
-]
